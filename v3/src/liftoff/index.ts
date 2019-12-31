4ad232fb7b139cb5fc89dbf95fb1972bbdc61811
Ashi Krishnan <queerviolet@github.com>
2019-12-30 21:47:27 -0500

    Prune the rest of Liftoff from the branch.

--- v3/src/liftoff/index.ts
+++
@@ -1,4 +0,0 @@
-export {Ref, isRef, getLabel, getTypeLabel, str, int, float, bool, obj} from './ref'
-export {Location, getLocation} from './loc'
-export {def} from './pattern'
-export {Core, Plan} from './core'
