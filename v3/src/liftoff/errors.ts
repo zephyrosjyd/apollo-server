586dbec0145c0e17a3df1af203f8d099860d2b21
Ashi Krishnan <queerviolet@github.com>
2019-12-30 21:45:13 -0500

    Manicure the error reporting.

--- v3/src/liftoff/errors.ts
+++ v3/src/liftoff/errors.ts
@@ -1,48 +1,114 @@
-import { Class } from '../utilities/types'
+import { Class, Instance, isTemplateInvocation } from '../utilities/types'
 
-export interface Thrower<C extends Class<Error>> {
-  /**
-   * Create and throw an error.
-   */
-  (...args: ConstructorParameters<C>): never
+export type Message<P extends any[]> = [string] | [(...params: P) => string] | [TemplateStringsArray, ...any[]]
 
+export type Fail<P extends any[], Base extends Error> = Base & {
   readonly code: string
+  readonly params: P
+}
 
-  /**
-   * Reference to the underlying class.
-   */
-  readonly class: C
+export interface FailureMode<P extends any[] = [object?], Base extends Error = Error> {
+  (...params: P): Failure<P, Base>
+  new (...params: P): Fail<P, Base>
+  readonly code: string
+  message: <MoreProps=undefined>(...msg: Message<[MoreProps]>) =>
+    P extends []
+      ? MoreProps extends object
+        ? FailureMode<[MoreProps], Base>
+        : FailureMode<P, Base>
+      :
+    P extends [infer Props]
+      ? MoreProps extends object
+        ? FailureMode<[Props & MoreProps], Base>
+        : FailureMode<P, Base>
+      :
+      FailureMode<P, Base>
+}
+
+export interface Failure<P extends any[], Base extends Error> {
+  create(): Fail<P, Base>
+}
+
+export class Failure<P extends any[], Base extends Error> {
+  constructor(
+    public readonly FailureMode: FailureMode<P, Base>,
+    public readonly params: P) {}
+
+  create(): Fail<P, Base> {
+    return new this.FailureMode(...this.params)
+  }
 }
 
 export interface ErrorWithStaticCode extends Class<Error> {
   readonly code: string
 }
 
-export function throws(code: string, message: string, Base?: Class<Error>): Thrower<Class<Error>>
-export function throws<E extends ErrorWithStaticCode>(ErrorClass: E): Thrower<E>
+export function fail
+  (code: string): FailureMode<[]>
+export function fail<P extends any[], M extends FailureMode<P, any>>
+  (code: string, mode: M): M
+export function fail<E extends Class<Error>>
+  (code: string, Base: E): FailureMode<ConstructorParameters<E>, Instance<E>>
+export function fail<E extends ErrorWithStaticCode>
+  (Base: E): FailureMode<ConstructorParameters<E>, Instance<E>>
 
-export function throws<E extends ErrorWithStaticCode>(
-  codeOrClass: E | string,
-  defaultMessage?: string,
-  Base: Class<Error> = Error
+export function fail<E extends ErrorWithStaticCode>(
+  codeOrBase: E | string,
+  ...rest: any[]
 ): any {
-  const ErrorClass = typeof codeOrClass === 'function'
-    ? codeOrClass
-    : class extends Base {
-      static readonly code = codeOrClass
-      constructor(message?: string, ...rest: any[]) {
-        super(`${message ?? defaultMessage} (${codeOrClass})`, ...rest)
-      }
-    }
+  let code: string
+  let Base: Class<Error>
+  if (typeof codeOrBase === 'string') {
+    code = codeOrBase
+    Base = rest[1] || Error
+  } else {
+    Base = rest[0]
+    code = codeOrBase.code
+  }
 
-    if (!('code' in ErrorClass.prototype))
-      Object.defineProperty(ErrorClass.prototype, 'code', {
-        value: ErrorClass.code, writable: true
-      })
-
-  return Object.assign(
-    function(...args: ConstructorParameters<E>): never {
-      throw new ErrorClass(...args)
-    }, { class: ErrorClass, code: ErrorClass.code }) as Thrower<E>
+  return failureMode(Base, code)
 }
 
+function formatMsg<P extends any[]>(message: Message<P>, params: P) {
+  if (!message.length) return null
+  if (isTemplateInvocation(message)) return String.raw(...message)
+  const [format] = message
+  if (typeof format === 'function') return format(...params)
+  if (typeof format === 'string') return format
+  return null
+}
+
+function failureMode<B extends Class<Error>>(Base: B, code: string, message?: Message<ConstructorParameters<B>>): FailureMode<ConstructorParameters<B>, Instance<B>> {
+  class FailureClass extends Base {
+    static readonly code = code
+    readonly params: ConstructorParameters<B>
+
+    static message(...msg: any) {
+      return failureMode(this, code, msg)
+    }
+
+    constructor(...params: any[]) {
+      super(...params)
+      this.params = params as any
+    }
+
+    get message() {
+      if (!message) return super.message
+      return [super.message, formatMsg(message, this.params)].filter(Boolean).join('\n\n')
+    }
+  }
+
+  function FailureMode(this: any, ...args: ConstructorParameters<B>) {
+    if (new.target)
+      return FailureClass.apply(this, args)
+    return new Failure(FailureMode as any, args)
+  }
+
+  Object.setPrototypeOf(FailureMode, FailureClass)
+  Object.defineProperties(FailureMode, { name: { value: code } })
+
+  FailureMode.prototype = Object.create(FailureClass.prototype)
+  FailureMode.prototype.constructor = FailureMode
+
+  return FailureMode as any
+}
